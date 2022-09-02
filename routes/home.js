b66cd788f6b1b31b642899c2998a21eb7cfcd29a
glopzel <74939915+lozzel@users.noreply.github.com>
2022-09-02 01:19:21 -0400

    added auth and views

--- routes/home.js
+++ routes/home.js
@@ -1,7 +1,19 @@
 const express = require('express')
 const router = express.Router()
 const homeController = require('../controllers/homeControl')
+// auth import
+const authController = require('../controllers/auth')
+const { ensureAuth, ensureGuest } = require('../middlewares/auth')
 
 router.get('/', homeController.getIndex)
+// auth matters, on controllers => auth
+// LOGIN
+router.get('/login', authController.getLogin)
+router.post('/login', authController.postLogin)
+// LOGOUT
+router.get('/logout', authController.logout)
+// SIGN UP
+router.get('/signup', authController.getSignup)
+router.post('/signup', authController.postSignup)
 
 module.exports = router
